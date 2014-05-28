3603f6cebb9fc86b5b84822bdb2e8bed55f3fea0
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-05-28 12:00:38 +0200

    Fixes to build in Windows

--- RawlogPlay/main.cpp
+++ RawlogPlay/main.cpp
@@ -2,8 +2,6 @@
    |                 Open MORA (MObile Robot Arquitecture)                     |
    +---------------------------------------------------------------------------+ */
 
-#include "CRawlogPlayApp.h" // User C++ app class 
-
 #define MORA_APP_CLASS         CRawlogPlayApp     
 #define MORA_APP_NAME          "RawlogPlay"    // Default MOOSApp app name
 
